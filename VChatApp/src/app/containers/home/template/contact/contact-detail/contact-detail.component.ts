406043483c17c996eb800d4387eb85157dcb64a3
Toan290501 <toandepzai290501@gmail.com>
2023-05-26 22:29:07 +0700

    all

---
+++ VChatApp/src/app/containers/home/template/contact/contact-detail/contact-detail.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
+
+declare const $: any;
+
+@Component({
+  selector: 'app-contact-detail',
+  templateUrl: './contact-detail.component.html',
+  styleUrls: ['./contact-detail.component.css']
+})
+export class ContactDetailComponent implements OnInit {
+  @Input() contact!: any;
+
+  toggleTabChat: boolean = false;
+  constructor(
+   
+  ) { }
+
+  ngOnInit() {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if ("contact" in changes) {
+      this.toggleTabChat = false;
+    }
+  }
+
+  chat() {
+    this.toggleTabChat = true;
+  }
+
+
+}
---
+++ VChatApp/src/app/core/model/message.ts
@@ -0,0 +1,13 @@
+import { User } from "./user";
+
+export interface Message {
+    Id: number;
+    Type: string;
+    GroupCode: string;
+    Content: string;
+    Path: string;
+    Created: Date;
+    CreatedBy: string;
+    SendTo: string;
+    UserCreatedBy: User;
+}
